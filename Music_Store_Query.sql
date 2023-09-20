f5093f1c9b60cab66d544045d8b0f2ed012bd49e
jhajhariadishita11 <109574200+jhajhariadishita11@users.noreply.github.com>
2023-09-20 08:30:26 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -66,16 +66,6 @@ WHERE track_id IN(
 ORDER BY email;
 
 
-/* Method 2 */
-
-SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
-FROM customer
-JOIN invoice ON invoice.customer_id = customer.customer_id
-JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
-JOIN track ON track.track_id = invoiceline.track_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-ORDER BY email;
 
 
 /* Q2: Let's invite the artists who have written the most rock music in our dataset. 
